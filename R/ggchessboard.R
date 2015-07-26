45b8445c42f4b08073e85f42f81b5beb1a0d795a
Joshua <jbkunst@gmail.com>
2015-07-26 19:49:25 -0300

    adding perspective in ggchessboard

--- R/ggchessboard.R
+++ R/ggchessboard.R
@@ -1,30 +1,35 @@
 #' @examples
-#' ggchessboard()
-#' p <- ggchessboard(fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2", c("#CCCCCC", "#FAFAFA"))
+#' ggchessboard(cellcols = c("#CCCCCC", "#FAFAFA"), piecesize = 17)
+#'
+#' ggchessboard(fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2")
 #'
 #' ggchessboard(fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2", perspective = "black")
 #'
 ggchessboard <- function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
                          cellcols = c("#D2B48C", "#F5F5DC"),
-                         perspective = "white"){
+                         perspective = "white",
+                         piecesize = 15){
 
   stopifnot(perspective %in% c("white", "black"))
 
   library("ggplot2")
+  library("dplyr")
 
   dchess <- chessboarddata(fen = fen)
 
-  p <- ggplot(dchess, aes(x, y))
+  str(dchess)
 
-  p <- p +
+  lvls <- if (perspective == "white") 1:8 else 8:1
+
+  dchess <- dchess %>%
+    mutate(x = factor(x, levels = lvls),
+           y = factor(y, levels = lvls))
+
+  p <- ggplot(dchess, aes(x, y)) +
     geom_tile(aes(fill = cc)) + scale_fill_manual(values = cellcols) +
-    geom_text(aes(label = text), size = 10)
+    geom_text(aes(label = text), size = piecesize) +
+    coord_equal() + theme_null()
 
-#
-#   if (perspective == "black") {
-#     p <- p + scale_x_reverse()
-#   }
-
-  p <- p + coord_equal() + theme_null()
+  p
 
 }
