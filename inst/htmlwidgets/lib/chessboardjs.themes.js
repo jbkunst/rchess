15708ae294a6a297d8e17fe3f66d82c9ac130beb
Joshua <jbkunst@gmail.com>
2015-10-05 15:19:27 -0300

    addgin chessboardjs and chessboardjs-themes

---
+++ inst/htmlwidgets/lib/chessboardjs.themes.js
@@ -0,0 +1,6 @@
+alpha_theme = function(piece){ return chssbrdths["alpha"][piece][0]; };
+chess24_theme = function(piece){ return chssbrdths["chess24"][piece][0]; };
+dilena_theme = function(piece){ return chssbrdths["dilena"][piece][0]; };
+symbol_theme = function(piece){ return chssbrdths["symbol"][piece][0]; };
+uscf_theme = function(piece){ return chssbrdths["uscf"][piece][0]; };
+wikipedia_theme = function(piece){ return chssbrdths["wikipedia"][piece][0]; };
