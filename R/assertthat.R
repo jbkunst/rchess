5c3fae55c78e806b530e6914562459cc4f296d42
Joshua <jbkunst@gmail.com>
2015-10-05 18:18:42 -0300

    is_valid_fen

--- R/assertthat.R
+++ R/assertthat.R
@@ -1,3 +1,37 @@
+is_valid_fen <- function(x) {
+  assertthat::is.string(x)
+  ct <- .get_context_chess_from_fen()
+  ct$assign("fen", x)
+  val <- ct$get("chess.validate_fen(fen)")
+  val$valid
+}
+
+assertthat::on_failure(is_valid_fen) <- function(call, env) {
+  x <- call$x
+  ct <- .get_context_chess_from_fen()
+  ct$assign("fen", x)
+  val <- ct$get("chess.validate_fen(fen)")
+  paste0(x, " ",val$error)
+}
+
+# assertthat::assert_that(is_valid_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+# assertthat::assert_that(is_valid_fen("asdas"))
+
+
+
+
+is_valid_move <- function(x, mvs){
+  x %in% mvs
+}
+
+assertthat::on_failure(is_valid_move) <- function(call, env) {
+  paste0(call$x, " is not a posible move")
+}
+
+# assertthat::assert_that(is_valid_move('a5', c('a5', 'd4')))
+# assertthat::assert_that(is_valid_move('a5', c('a6', 'd4')))
+
+
 is_chess_square <- function(x){
   squares <- expand.grid(letters[seq(8)], seq(8))
   squares <- paste0(squares$Var1, squares$Var2)
@@ -8,5 +42,5 @@ assertthat::on_failure(is_chess_square) <- function(call, env) {
   paste0(deparse(call$x), " is not a valid chess square")
 }
 
-# assert_that(is_chess_square('a5'))
-# assert_that(is_chess_square('s5'))
+# assertthat::assert_that(is_chess_square('a5'))
+# assertthat::assert_that(is_chess_square('s5'))
