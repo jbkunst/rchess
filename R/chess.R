60cf01bf82343e9fdf4c9171d59da2d182ee021e
Joshua <jbkunst@gmail.com>
2015-10-05 12:00:33 -0300

    WHOA! So :smile:  about this!

--- R/chess.R
+++ R/chess.R
@@ -6,20 +6,75 @@
 #' @export
 Chess <- R6::R6Class(
   "Chess",
+  private = list(
+    ct = NULL
+  ),
   public = list(
-    fen = NA,
-    initialize = function(fen) {
-      self$fen <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    initialize = function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
+      self$init_ct(fen)
     },
+    init_ct = function(fen){
+      private$ct <- .get_context_chess_from_fen(fen)
+    },
+    # chessjs api
     ascii = function(){
-      .print_chess(self$fen)
+      cat(private$ct$get("chess.ascii()"))
     },
-    moves = function(){
-      .get_moves(self$fen)
+    clear = function(){
+      private$ct$eval(V8::JS("chess.clear()"))
+      invisible(self)
     },
-    print = function(){
-      .print_chess(self$fen)
-    }))
+    fen = function(){
+      private$ct$get(V8::JS("chess.fen()"))
+    },
+    game_over = function(){
+      private$ct$get(V8::JS("chess.game_over()"))
+    },
+    get = function(square){},
+    history = function(options){},
+    in_check = function(){
+      private$ct$get(V8::JS("chess.in_check()"))
+    },
+    in_checkmate = function(){
+      private$ct$get(V8::JS("chess.in_checkmate()"))
+    },
+    in_draw = function(){
+      private$ct$get(V8::JS("chess.in_draw()"))
+    },
+    in_stalemate = function(){
+      private$ct$get(V8::JS("chess.in_stalemate()"))
+    },
+    in_threefold_repetition = function(){
+      private$ct$get(V8::JS("chess.in_threefold_repetition()"))
+    },
+    header = function(){},
+    insufficient_material = function(){
+      private$ct$get(V8::JS("chess.insufficient_material()"))
+    },
+    load = function(fen){},
+    load_pgn = function(pgn, options){},
+    move = function(move){
+      strg <- sprintf("chess.move('%s')", move)
+      private$ct$eval(V8::JS(strg))
+      invisible(self)
+    },
+    moves = function(options){
+      moves <- private$ct$get("chess.moves()")
+      moves
+    },
+    pgn = function(options){},
+    put = function(piece, square){},
+    remove = function(square){},
+    reset = function(){},
+    square_color = function(square){},
+    turn = function(){},
+    undo = function(){},
+    validate_fen = function(fen){},
+
+    # generic methods
+    summary = function(){ cat("summary Chess object")},
+    plot    = function(){ cat("plot Chess object")},
+    print   = function(){ cat("print Chess object") }))
 
 
 .get_context_chess_from_fen <- function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
@@ -27,24 +82,19 @@ Chess <- R6::R6Class(
   ct$source(system.file("htmlwidgets/lib/chess.min.js", package = "rchess"))
   ct$assign("fen", fen)
   ct$assign("chess", V8::JS("new Chess(fen);"))
-
   ct
 }
 
-.print_chess <- function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
 
-  ct <- .get_context_chess_from_fen(fen)
-  cat(ct$get("chess.ascii()"))
-  invisible()
-
+summary.Chess <- function(x, ...) {
+  x$summary()
 }
 
-.get_moves <- function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
-
-  ct <- .get_context_chess_from_fen(fen)
-  moves <- ct$get("chess.moves()")
-  moves
-
+plot.Chess <- function(x, ...) {
+  x$plot()
 }
 
+print.Chess <- function(x, ...) {
+  x$print()
+}
 
--- tests/test.R
+++ tests/test.R
@@ -1,11 +1,46 @@
 library(rchess)
+rm(list = ls())
 
+#### Creating Chess object ####
 chss <- Chess$new()
 
-chss
+class(chss)
 
 chss$ascii()
+
 chss$moves()
-chss$fen
+
+chss$move("a3")
+
+chss$ascii()
+
+chss$move("e5")$move("f4")
+
+chss$ascii()
+
+chss$fen()
+
+
+### Other example
+chss2 <- Chess$new("rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3")
+
+chss2$ascii()
+
+chss2$in_check()
+
+chss2$in_checkmate()
+
+
+### Slatemate
+chss3 <- Chess$new("4k3/4P3/4K3/8/8/8/8/8 b - - 0 78")
+
+chss3$in_stalemate()
+
+#### Methods ####
+summary(chss)
+
+plot(chss)
+
+print(chss)
 
 
