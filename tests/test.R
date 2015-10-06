0d55539a3a63b89a5d0dfa2c1b53f88b9aab8f21
Joshua <jbkunst@gmail.com>
2015-10-06 18:39:48 -0300

    updating tests

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,148 @@
+#' ---
+#' title: "Presenting `rchess` package"
+#' author: "Joshua Kunst"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    keep_md: yes
+#' ---
+
 library(rchess)
 rm(list = ls())
+
+#' ## Basic Usage
+chss <- Chess$new()
+
+chss
+
+chss$moves()
+
+chss$moves(verbose = TRUE)
+
+chss$move("a3")
+
+#' We can concate some moves and a capture
+chss$move("e5")$move("f4")$move("Qe7")$move("fxe5")
+
+plot(chss)
+
+chss$turn()
+
+chss$square_color("h1")
+
+chss$get("e5")
+
+chss$history(verbose = TRUE)
+
+chss$history()
+
+chss$undo()
+
+chss$history()
+
+chss$fen()
+
+chss$header("White", "You")
+
+chss$header("Black", "Me")
+
+cat(chss$pgn())
+
+chss$ascii()
+
+#' ## Generic Methods
+#'
+#' ### Summary
+summary(chss)
+
+#' ### Plot
+#'
+#' Via `chessboardjs` htmlwidget implementation
+plot(chss)
+
+#' If you dont like so much `chessboarjs` you can do vía ggplot2 with the `ggchessboard` function.
+plot(chss, type = "ggplot")
+plot(chss, type = "ggplot", cellcols = c("#CCCCCC", "#FAFAFA"), piecesize = 13, perspective = "black")
+
+#' If you don't have a Chess object but a fen string you can do:
+fen <- "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+
+chessboardjs(fen)
+
+ggchessboard(fen)
+
+#' ### Print
+#'
+#' Sames as summary (by now)
+print(chss)
+
+#' ## Other Functions
+#'
+#' ### Load Fen
+chss2 <- Chess$new()
+
+chss2$plot()
+
+fen <- "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+
+chss2$load(fen)
+
+chss2$plot()
+
+#' ### Load PGN
+pgn <- system.file("data/kasparov_vs_topalov.pgn", package = "rchess")
+pgn <- readLines(pgn, warn = FALSE)
+pgn <- paste(pgn, collapse = "\n")
+cat(pgn)
+
+chsspgn <- Chess$new()
+
+chsspgn$load_pgn(pgn)
+
+cat(chsspgn$pgn())
+
+chsspgn$history()
+
+head(chsspgn$history(verbose = TRUE), 6)
+
+
+#' ### State validation
+chss2 <- Chess$new("rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3")
+
+plot(chss2)
+
+chss2$in_check()
+
+chss2$in_checkmate()
+
+#' ### Slatemate validation
+chss3 <- Chess$new("4k3/4P3/4K3/8/8/8/8/8 b - - 0 78")
+
+plot(chss3)
+
+chss3$in_stalemate()
+
+#' ### 3 fold repetition
+chss4 <- Chess$new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+chss4$in_threefold_repetition()
+
+chss4$move('Nf3')$move('Nf6')$move('Ng1')$move('Ng8')
+chss4$in_threefold_repetition()
+
+chss4$move('Nf3')$move('Nf6')$move('Ng1')$move('Ng8')
+chss4$in_threefold_repetition()
+
+chss4$history()
+
+#' ### Insufficient material
+chess5 <- Chess$new("k7/8/n7/8/8/8/8/7K b - - 0 1")
+plot(chess5)
+chess5$insufficient_material()
+
+
+
+
 
 ### INTERNAL ###
 # ct <- rchess:::.get_context_chess_from_fen()
@@ -12,91 +155,3 @@ rm(list = ls())
 # ct$get("chess.history({ verbose: verb})")
 # ct$assign("fen", "no fen")
 # ct$get("chess.validate_fen(fen)")
-
-#### Creating Chess object ####
-chss <- Chess$new()
-
-class(chss)
-
-chss$ascii()
-
-chss$moves()
-
-chss$move("a3")
-
-chss$ascii()
-
-chss$move("e5")$move("f4")
-
-chss$ascii()
-
-chss$fen()
-
-chss$get("e5")
-
-chss$history()
-
-chss$history(verbose = TRUE)
-
-
-
-
-#### Methods ####
-summary(chss)
-
-#' Via ggchessboard function
-plot(chss)
-
-#' Via ggchessboard function
-plot(chss, cellcols = c("#CCCCCC", "#FAFAFA"), piecesize = 17, perspective = "black")
-
-#' Via chessboardjs htmlwidget implmentation
-plot(chss, type = "chessboardjs", width = 400, height = 400)
-
-#' Or just ascii
-plot(chss, type = "ascii")
-
-
-print(chss)
-
-### Example validation FEN ###
-chsserror <- Chess$new(fen = "something is not fen")
-
-chss$validate_fen("asda")
-
-#### Example State validation #####
-chss2 <- Chess$new("rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3")
-
-chss2$ascii()
-
-chss2$in_check()
-
-chss2$in_checkmate()
-
-
-#### Example Slatemate #####
-chss3 <- Chess$new("4k3/4P3/4K3/8/8/8/8/8 b - - 0 78")
-
-chss3$in_stalemate()
-
-#### Example 3 fold rep #####
-chss4 <- Chess$new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-chss4
-
-chss4$in_threefold_repetition()
-
-chss4$move('Nf3')$move('Nf6')$move('Ng1')$move('Ng8')
-chss4$fen()
-chss4$in_threefold_repetition()
-
-chss4$move('Nf3')$move('Nf6')$move('Ng1')$move('Ng8')
-chss4$fen()
-chss4$in_threefold_repetition()
-
-#### Example insu material
-chess5 <- Chess$new("k7/8/n7/8/8/8/8/7K b - - 0 1")
-chess5$insufficient_material()
-
-
-
-
