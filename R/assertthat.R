4bafa90e0a6bc0d52f49c5afdfa024ce2a73db8b
Joshua <jbkunst@gmail.com>
2015-10-29 14:10:22 -0300

    removing scripts and comments

--- R/assertthat.R
+++ R/assertthat.R
@@ -15,10 +15,6 @@ assertthat::on_failure(is_valid_fen) <- function(call, env) {
   paste0(x, " ",val$error)
 }
 
-# assertthat::assert_that(is_valid_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
-# assertthat::assert_that(is_valid_fen("asdas"))
-
-
 is_valid_move <- function(x, mvs){
   x %in% mvs
 }
@@ -27,10 +23,6 @@ assertthat::on_failure(is_valid_move) <- function(call, env) {
   x <- call$x
   paste0(x, " is not a posible move")
 }
-
-# assertthat::assert_that(is_valid_move('a5', c('a5', 'd4')))
-# assertthat::assert_that(is_valid_move('a5', c('a6', 'd4')))
-
 
 is_chess_square <- function(x){
   squares <- expand.grid(letters[seq(8)], seq(8))
@@ -41,6 +33,3 @@ is_chess_square <- function(x){
 assertthat::on_failure(is_chess_square) <- function(call, env) {
   paste0(deparse(call$x), " is not a valid chess square")
 }
-
-# assertthat::assert_that(is_chess_square('a5'))
-# assertthat::assert_that(is_chess_square('s5'))
