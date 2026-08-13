76c637685782bf5d4199f0eb2a5de62e6f2dbf26
Joshua Kunst <jbkunst@gmail.com>
2026-08-13 17:37:27 -0400

    Cover castling path in history detail

--- tests/testthat/test-modern-dplyr.R
+++ tests/testthat/test-modern-dplyr.R
@@ -14,3 +14,13 @@ test_that("verbose methods work after loading PGN", {
   expect_named(history, c("color", "from", "to", "flags", "piece", "san", "number_move"))
   expect_named(detail, c("piece", "from", "to", "number_move", "piece_number_move", "status", "number_move_capture", "captured_by"))
 })
+
+test_that("history detail handles castling", {
+  game <- do.call(Chess[["new"]], list())
+  do.call(game[["load_pgn"]], list("1. e4 e5 2. Nf3 Nc6 3. Bc4 Nf6 4. O-O"))
+
+  detail <- do.call(game[["history_detail"]], list())
+
+  expect_true(tibble::is_tibble(detail))
+  expect_named(detail, c("piece", "from", "to", "number_move", "piece_number_move", "status", "number_move_capture", "captured_by"))
+})
